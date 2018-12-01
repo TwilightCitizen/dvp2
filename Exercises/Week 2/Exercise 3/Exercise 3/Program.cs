be17421932bc031a9c61f4a90a3c09fe0208813b
David Clark <daclark1@student.fullsail.edu>
2018-12-01 18:52:31 -0500

    Modified the Exercise to Support Undo/Redo.
    This was more challenging than expected due to how I implemented it in the previous exercise, which did not require deep copy of objects.  This exercise did require deep copy of objects, which seemed ill-fit considering the scope.  Instead, Undo/Redo now captures Actions that can be played to affect a desired state.  This puts some cross-cutting concerns on the application that can perhaps be abstracted away better, but at least the present implementation works without need for deep copy.

--- Exercises/Week 2/Exercise 3/Exercise 3/Program.cs
+++ Exercises/Week 2/Exercise 3/Exercise 3/Program.cs
@@ -90,6 +90,15 @@ namespace Exercise_3
             )
         };
 
+        /* These don't work as expected for this use case.  More below in comments.
+        // Stacks for Undo/Redo Support.  Somewhere, there's an abstraction
+        // brewing to handle this more cleanly.
+        private static Stack< List< Student > > undos = new Stack< List< Student > >();
+        private static Stack< List< Student > > redos = new Stack< List< Student > >(); */
+
+        private static Stack< ( Action, Action ) > opUndos = new Stack< ( Action, Action ) >();
+        private static Stack< ( Action, Action ) > opRedos = new Stack< ( Action, Action ) >();
+
         // Menu of Students to Review
         private static SuperMenu menuStudents = new SuperMenu
         (
@@ -106,6 +115,39 @@ namespace Exercise_3
         ,   new ActionSubMenuOption(
                 "Edit Students",        LoadStudentsEdit,     menuStudents
             )
+        
+        /* Yikes! Undo as originally implemented in the previous exercise
+         * doesn't work so easily here. More information below in the
+         * implementation comments
+         * 
+        ,   new ActionOnlyOption( "Undo the Last Action"
+                                , Undo
+                                , conditions : new List< Func< bool > >()
+                                               {
+                                                   () => undos.Any()
+                                               } )
+
+        ,   new ActionOnlyOption( "Redo the Last undone Action"
+                                , Redo
+                                , conditions : new List< Func< bool > >()
+                                               {
+                                                   () => redos.Any()
+                                               } ) */
+
+        // Setup for more workable--I hesitate to say better--undo.
+        ,   new ActionOnlyOption( "Undo the Last Action"
+                                , OpUndo
+                                , conditions : new List< Func< bool > >()
+                                               {
+                                                    () => opUndos.Any()
+                                               } )
+
+        ,   new ActionOnlyOption( "Redo the Last Undone Action"
+                                , OpRedo
+                                , conditions : new List< Func< bool > >()
+                                               {
+                                                    () => opRedos.Any()
+                                               } )
 
         ,   new ActionOnlyOption(
                 "View Students GPAs",   ViewGPAs
@@ -199,13 +241,17 @@ namespace Exercise_3
         // Prompt the user for the Student's new Percent Grade for the given course.
         private static void EditCourse( string name, TakenCourse course )
         {
+            // This is the only destructive operation to the student's information, which
+            // can benefit from undo/redo-ability.
+            // PrepUndoRedo(); // Doesn't work as envisioned. More in implementation comments.
+
             // Get the new Percent Grade from the user and update the Taken Course with it.
             // Finally, this demonstrates the utility of PromptFor< T > from my utilities
             // class library.  PromptFor< T > takes as arugments a prompt to display to the user,
             // an error prompt to display in case of bad input, a delegate expression with which
             // to test for constraints beyond type, and of course, the type to which the user's
             // input should be converted from string.
-            course.Grade.Value = PromptFor< decimal >
+            decimal newGrade = PromptFor< decimal >
             (
                 $"What is the new percent grade for {name}'s {course.Name}?\n"
             +   "This must be between 0.00 and 100.00 percent.  Do not include\n"
@@ -214,6 +260,27 @@ namespace Exercise_3
             ,   input => input >= PercentGrade.MinPercent
                       && input <= PercentGrade.MaxPercent 
             );
+
+            // This is a little different than originally planned.  The previous statement once
+            // updated course.Grade.Value directly, but we can't do this with the operative undo
+            // and redo facilities.  So, we capture the new grade, capture the old grade, and
+            // wrap them in closures along with the current references to course.Grade.Value that
+            // can be played at any time to affect the value.
+            decimal oldGrade = course.Grade.Value;
+
+            // Closures.
+            Action OldGrade = () => { course.Grade.Value = oldGrade; };
+            Action NewGrade = () => { course.Grade.Value = newGrade; };
+
+            // Prepare to undo/redo this change.
+            OpPrepUndoRedo( OldGrade, NewGrade );
+
+            // Then update the Grade in earnest.
+            NewGrade();
+
+            // That whole sequence to prepare for Undo/Redo without worrying about Deep Copying
+            // objects/collections has it's own considerations for abstraction.  As it stands
+            // right now, this is a thorny cross-cutting concern, but it should work.
 
             // Let the user know the edit happened.
             Console.WriteLine( $"{name}'s {course.Name} has been updated to {course.Grade.Value}%" );
@@ -237,6 +304,82 @@ namespace Exercise_3
             // Let the user know to hit a key to continue.
             Console.WriteLine( "\nPress any key to continue..." );
             Console.ReadKey();
+        }
+
+        /* So... Merely copying students into a new list before adding it
+         * to the undo/redo stack does create new Student objects with their
+         * own references.  BUT(!) its a shallow copy.  These new Student
+         * objects still carry references to the underlying objects, namely
+         * the Taken Courses.  Even worse!  C# has no native way to do a
+         * Deep Copy.  Nope... You have to use serialization or reflection
+         * within an extension method to come close.  This is terribly
+         * lacking.
+         * 
+         * One way I see around this is to have an undo stack onto which
+         * Actions are pushed by the programmer that operatively specify
+         * how to undo the action about to be done.  I'm going to leave
+         * this commented in to demonstrate learning and code in the
+         * other way to do undo to see if that works.
+         
+        // Push a copy of the Student onto the Undo stack and clear
+        // the Redo stack.  A copy is necessary since lists are ref types.
+        private static void PrepUndoRedo()
+        {
+            undos.Push( new List< Student >( students ) );
+            redos.Clear();
+        }
+
+        // Undo just pops the last pushed Student list from the undo
+        // stack into Students.  To support redoing the undone
+        // action, it pushes the current Students to the redoStack.
+        private static void Undo()
+        {
+            redos.Push( new List< Student >( students ) );
+            students = undos.Pop();
+
+            // Let the user know the last action is undone.
+            Console.WriteLine( "\nThe students have been restored to their previous state." );
+        }
+
+        // Undo the undo.  This can only be done if something hasn't been done
+        // after an undo. 
+        private static void Redo()
+        {
+            undos.Push( new List< Student >( students ) );
+            students = redos.Pop();
+
+            // Let the user know the last undone action is redone.
+            Console.WriteLine( "\nThe students have been restored to their previous undone state." );
+        } */
+
+        // Push the actions onto the undo stack that restore to the current state,
+        // and affect the projected state.
+        private static void OpPrepUndoRedo( Action current, Action projected )
+        {
+            opUndos.Push( ( current, projected ) );
+            opRedos.Clear();
+        }
+
+        // Pop the actions off the undo stack that restore the previous state and
+        // redo the current state. Redo is an undo of the undo, so the reverse
+        // gets pushed to the redo stack. Finally, restore the previous state
+        // by executing the action that restores it.
+        private static void OpUndo()
+        {
+            var ( previous, current ) = opUndos.Pop();
+            opRedos.Push( ( current, previous ) );
+            previous();
+        }
+
+        // This essentially does the same as undo, except opUndos and opRedos
+        // trade roles.  Technically, looking at the two methods, these could
+        // be further abstracted, but the resulting code would be confusing as
+        // all get out.
+        private static void OpRedo()
+        {
+            var ( previous, current ) = opRedos.Pop();
+            opUndos.Push( ( current, previous ) );
+            previous();
         }
 
         // Program entry point.
